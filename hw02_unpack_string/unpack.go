1b9418412c74d564fcba2ac9fe5ee9d5f612299b
Илья Киселев <ikiselev@diasoft.ru>
2025-05-12 14:15:51 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
+var ErrInternal = errors.New("internal error")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+	if unicode.IsDigit(rune(s[0])) {
+		return "", ErrInvalidString
+	}
+
+	var builder strings.Builder
+	var last rune
+
+	for i, current := range s {
+		if unicode.IsLetter(current) || unicode.IsSpace(current) {
+			last = current
+			builder.WriteRune(current)
+		} else if unicode.IsDigit(current) {
+			if unicode.IsDigit(rune(s[i-1])) {
+				return "", ErrInvalidString
+			}
+			count, err := strconv.Atoi(string(current))
+			if err != nil || count < 0 {
+				return "", ErrInternal
+			}
+			if count == 0 {
+				result := builder.String()
+				builder.Reset()
+				builder.WriteString(result[:len(result)-1])
+			} else {
+				builder.WriteString(strings.Repeat(string(last), count-1))
+			}
+		}
+	}
+	return builder.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,8 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "aaab0", expected: "aaa"},
+		{input: " aaa", expected: " aaa"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -40,6 +42,17 @@ func TestUnpackInvalidString(t *testing.T) {
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackErrInternal(t *testing.T) {
+	invalidStrings := []string{"ab१c"}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInternal), "actual error %q", err)
 		})
 	}
 }
