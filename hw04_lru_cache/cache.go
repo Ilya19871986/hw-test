e1f39953dcad210323893b80bf12686eb64961fc
Илья Киселев <ikiselev@diasoft.ru>
2025-07-09 16:16:21 +0300

    HW4 fix

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -14,20 +14,20 @@ type lruCache struct {
 	items    map[Key]*ListItem
 }
 
-// Set - добавляет или обновляет элемент в кэше
+// Set - добавляет или обновляет элемент в кэше.
 func (c *lruCache) Set(key Key, value interface{}) bool {
-	// Если ключ уже существует, обновляем значение и перемещаем в начало
+	// Если ключ уже существует, обновляем значение и перемещаем в начало.
 	if item, exists := c.items[key]; exists {
 		item.Value = value
 		c.queue.MoveToFront(item)
 		return true
 	}
 
-	// Если кэш полон, удаляем последний элемент
+	// Если кэш полон, удаляем последний элемент.
 	if c.queue.Len() >= c.capacity {
 		last := c.queue.Back()
 		if last != nil {
-			// Находим ключ, соответствующий последнему элементу
+			// Находим ключ, соответствующий последнему элементу.
 			for k, v := range c.items {
 				if v == last {
 					delete(c.items, k)
@@ -38,24 +38,24 @@ func (c *lruCache) Set(key Key, value interface{}) bool {
 		}
 	}
 
-	// Добавляем новый элемент в начало
+	// Добавляем новый элемент в начало.
 	listItem := c.queue.PushFront(value)
 	c.items[key] = listItem
 	return false
 }
 
-// Get - получает значение по ключу
+// Get - получает значение по ключу.
 func (c *lruCache) Get(key Key) (interface{}, bool) {
 	item, exists := c.items[key]
 	if !exists {
 		return nil, false
 	}
-	// Перемещаем элемент в начало
+	// Перемещаем элемент в начало.
 	c.queue.MoveToFront(item)
 	return item.Value, true
 }
 
-// Clear - очищает кэш
+// Clear - очищает кэш.
 func (c *lruCache) Clear() {
 	c.queue = NewList()
 	c.items = make(map[Key]*ListItem, c.capacity)
