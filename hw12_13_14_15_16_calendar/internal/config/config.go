9469f695c4799e4568f8eafc3f2c9387320f659b
Илья Киселев <ikiselev@diasoft.ru>
2026-01-13 09:42:57 +0300

    hw14 is completed

--- hw12_13_14_15_16_calendar/internal/config/config.go
+++ hw12_13_14_15_16_calendar/internal/config/config.go
@@ -2,14 +2,17 @@ package config
 
 import (
 	"os"
+	"time"
 
 	yaml "gopkg.in/yaml.v3"
 )
 
 type Config struct {
-	Server  ServerConfig  `yaml:"server"`
-	Logger  LoggerConfig  `yaml:"logger"`
-	Storage StorageConfig `yaml:"storage"`
+	Server    ServerConfig    `yaml:"server"`
+	Logger    LoggerConfig    `yaml:"logger"`
+	Storage   StorageConfig   `yaml:"storage"`
+	Kafka     KafkaConfig     `yaml:"kafka"`
+	Scheduler SchedulerConfig `yaml:"scheduler"`
 }
 
 type ServerConfig struct {
@@ -24,6 +27,20 @@ type LoggerConfig struct {
 type StorageConfig struct {
 	Type string `yaml:"type"`
 	DSN  string `yaml:"dsn"`
+}
+
+type KafkaConfig struct {
+	Brokers      []string      `yaml:"brokers"`
+	Topic        string        `yaml:"topic"`
+	GroupID      string        `yaml:"group_id"`
+	ClientID     string        `yaml:"client_id"`
+	MaxAttempts  int           `yaml:"max_attempts"`
+	RetryBackoff time.Duration `yaml:"retry_backoff"`
+}
+
+type SchedulerConfig struct {
+	Interval         time.Duration `yaml:"interval"`
+	CleanupOlderThan time.Duration `yaml:"cleanup_older_than"`
 }
 
 func LoadConfig(filename string) (*Config, error) {
